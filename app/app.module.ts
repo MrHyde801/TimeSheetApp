b5a579132ff1fbae1c1b235f99ddc88b0f4afab2
Ian Hyde <mrhyde801@Ians-MacBook-Pro.local>
2022-03-26 15:36:24 -0600

    Finished unit 13

---
+++ app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { environment } from 'src/environments/environment';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC6NN0iXPJW-SJbPrMAAFvIX8Z6Ymle_To",
+    authDomain: "timesheet-project-20d8b.firebaseapp.com",
+    projectId: "timesheet-project-20d8b",
+    storageBucket: "timesheet-project-20d8b.appspot.com",
+    messagingSenderId: "838901749467",
+    appId: "1:838901749467:web:0f66bd9add611d2266defe",
+    measurementId: "G-LDHCKC88Z5"
+  },
+};
